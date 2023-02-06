grembeter/dot

commit bd7ce9a3ac0cec57a0a637d080fa1dfac62eb930

zsh: improve terminal emulation function
It is used everyday, so make it easy to run:
  - short aliases
  - accept full path as tty device name

Signed-off-by: grygorii tertychnyi <grembeter@outlook.com>

dot.config/zsh/lib/main.zsh
@@ -25,8 +25,29 @@ g-chdir-parent() {
 #
 # run terminal emulation program with predefined device and baudrate
 #
-g-picocom() {
-    picog "/dev/ttyUSB${1:-0}" -b "${2:-115200}"
+g-ttyusb() {
+    local ttydev="$1"
+    local baudrate="${2:-115200}"
+
+    if echo "$ttydev" | grep -qEe '^[0-9]+$'; then
+        ttydev="/dev/ttyUSB$ttydev"
+    fi
+
+    # map LF to CR+LF after being read from the serial port
+    # map CR to LF before being written to the serial port
+    picocom --imap lfcrlf --omap crlf \
+            --logfile /tmp/picocom.$(basename "$ttydev").log \
+            "$ttydev"
+}
+
+#
+# list serial devices
+#
+g-ttyusbls() {
+    local ttydev
+    for ttydev in /dev/serial/by-id/*; do
+        echo "$(readlink -f $ttydev) # $ttydev"
+    done
 }
 
 #

dot.config/zsh/lib/aliases.zsh
@@ -13,6 +13,12 @@ alias 1g='sudo aptitude safe-upgrade'
 alias 2e='. ~/bin/env-bitbake'
 alias 2s='g-chdir-repo'
 
+alias 3='g-ttyusb'
+alias 3l='g-ttyusbls'
+alias 3o='lsof /dev/ttyUSB?'
+alias 3x='ls --color -l /tmp/picocom.*.log'
+alias 3xxx='rm -vf /tmp/picocom.*.log'
+
 alias 5s='repo sync --no-clone-bundle --verbose'
 
 alias cd='cd -P'
@@ -36,10 +42,6 @@ alias bat='batcat'
 
 alias g='git'
 alias gdif='git diff --no-index'
-alias picog='picocom --imap lfcrlf --omap crlf'
-
-alias ttyusb='ll /dev/ttyUSB?'
-alias ttyusbof='lsof /dev/ttyUSB?'
 
 alias reboot='sudo -k reboot'
 alias poweroff='sudo -k poweroff'

commit 04cbd2f25ba033f33414272f09465c98728f94ad

zsh: fix g-ttyusb
Baudrate value (second argument) is ignored and never passed to
"picocom", that always sets 9600 baud by default.

Hence, add "--baud" argument to the call.

Also, use "/dev/ttyUSB0" as the default tty device.

Fixes: bd7ce9a3ac0c ("zsh: improve terminal emulation function")
Signed-off-by: grygorii tertychnyi <grembeter@outlook.com>

dot.config/zsh/lib/main.zsh
@@ -26,7 +26,7 @@ g-chdir-parent() {
 # run terminal emulation program with predefined device and baudrate
 #
 g-ttyusb() {
-    local ttydev="$1"
+    local ttydev="${1:-0}"
     local baudrate="${2:-115200}"
 
     if echo "$ttydev" | grep -qEe '^[0-9]+$'; then
@@ -37,6 +37,7 @@ g-ttyusb() {
     # map CR to LF before being written to the serial port
     picocom --imap lfcrlf --omap crlf \
             --logfile /tmp/picocom.$(basename "$ttydev").log \
+            --baud "$baudrate" \
             "$ttydev"
 }
 